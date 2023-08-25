ab542a2badaa873f36e1a4fe05ebb40ac64dcb2a
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2023-08-25 16:28:47 -0300

    Update .py files

--- pyFiles/Config.py
+++ pyFiles/Config.py
@@ -1,3 +1,11 @@
+'''
+    CREATED BY: ALBERTO BRUNO SILVESTRE DE OLIVEIRA
+
+    GITHUB: https://github.com/AlbertoBruno1265/IIS-Logs-to-PostgreSQL-DataBase
+
+    Config.py
+'''
+
 # Settings
 database = ""
 user = ""
